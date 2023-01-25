3a472527169b042d1983b785bb1c98ffdbd12ed8
Pavel <pasha311115@yandex.ru>
2023-01-25 21:06:35 +0300

    delete card

--- routes/editCard.routes.js
+++ routes/editCard.routes.js
@@ -3,30 +3,29 @@ const { User } = require("../db/models");
 const { Card } = require("../db/models");
 const create = require("../views/EditCard");
 
-router.get('/:id', async (req, res) => {
+router.get("/", async (req, res) => {
   const { userId } = req.session;
-  const { id } = req.params;
-  const user = userId && (await User.findByPk(Number(userId)));
-  res.renderComponent(create, { user, id });
+  const user = await User.findOne({
+    where: { id: userId },
+  });
+  const Cards = await Card.findAll({ where: { user_id: userId } });
+  res.renderComponent(create, { user, Cards });
 });
-
-router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { title, description } = req.body;
-  const EditCard = await Card.update(
-    {
-      title,
-      description,
-    },
-    {
-      where: {
-        id,
-      },
-      returning: true,
-    }
-  );
-  if (EditCard[0] > 0) {
-    res.status(200).json({ editcard: true });
-  }
+router.post("/", async (req, res) => {
+  const { userId } = req.session;
+  const { img, name, quality, price } = req.body;
+  const newCard = await Card.create({
+    img,
+    name,
+    quality,
+    price,
+    user_id: userId,
+  });
+  res.redirect("/editcard");
+});
+router.delete("/:idCard", async (req, res) => {
+  const { idCard } = req.params;
+  const del = await Card.destroy({ where: { id: idCard } });
+  res.json({ key: del });
 });
 module.exports = router;
--- views/EditCard.jsx
+++ views/EditCard.jsx
@@ -1,24 +1,60 @@
-const React = require('react');
-const Layout = require('./Layout');
+const React = require("react");
+const Layout = require("./Layout");
 
-module.exports = function Create({ user, id }) {
+module.exports = function Cards({ user, title, Cards }) {
+  Cards = Cards.sort((a, b) => a.id - b.id);
   return (
-    <Layout user={user}>
+    <Layout title={title} user={user}>
+      <div className="catalog row container" id="card_list">
+        {Cards.map((el) => (
+          <div key={el.id} className="card catalog__card">
+            <img src={el.img} className="catalog__img" alt="img" />
+            <h5 className="catalog__title">{el.name}</h5>
+            <p className="catalog__text">{el.price}</p>
+            <p className="catalog__text">{el.quality}</p>
+            <a href={`/editbook/${el.id}`}>
+              <button type="submit" className="button">
+                Изменить
+              </button>
+            </a>
+
+            <button type="submit" data-id={el.id} className="button js-delete">
+              Удалить
+            </button>
+          </div>
+        ))}
+      </div>
       <div className="container">
-        <form action={`/editcard/${id}`} id="formEditCard">
-          <span>title</span>
+        <form action="/editcard" method="POST">
+          <span>Name</span>
           <input
             className="form-control mb-2 bg-light"
-            name="title"
+            name="name"
             id="TitleInput"
             type="text"
             required
           />
-          <span>Description</span>
+          <span>Price</span>
           <input
             className="form-control mb-2 bg-light"
-            name="description"
+            name="price"
             id="DescribeInput"
+            type="text"
+            required
+          />
+          <span>Img</span>
+          <input
+            className="form-control mb-2 bg-light"
+            name="img"
+            id="UrlInput"
+            type="text"
+            required
+          />
+          <span>Quality</span>
+          <input
+            className="form-control mb-2 bg-light"
+            name="quality"
+            id="UrlInput"
             type="text"
             required
           />
@@ -27,7 +63,7 @@ module.exports = function Create({ user, id }) {
             className="button btn-default btn-m bg-info"
             type="submit"
           >
-            Edit
+            Add Card
           </button>
         </form>
       </div>
